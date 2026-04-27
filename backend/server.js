485b84e6fe096f991970fc7013d96efb87874213
krostdev <augustokauanpe@gmail.com>
2026-04-27 16:05:01 -0300

    feat: Inicializando servidor e adicionando rota teste '/'

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
+const express = require("express");
+const dns = require("node:dns");
+const app = require("./src/app.js");
 
-const dns = require("node:dns")
+const PORT = process.env.PORT || 5050
+
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
-
 require("./src/config/database.js");
+
+app.listen(PORT, () => {
+    console.log(`[SERVER] Servidor iniciado na porta ${PORT}.`)
+});
